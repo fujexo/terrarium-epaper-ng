8012c329c21422653ca9291490acd5b7b5e03f98
Philipp Marmet <fujexo@c0d3.ch>
2024-02-16 13:39:53 +0100

    working nrf52840 myusb

--- src/bin/myusb.rs
+++ src/bin/myusb.rs
@@ -1,94 +1,40 @@
 #![no_std]
 #![no_main]
 
+use core::mem;
 use defmt::info;
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
 use embassy_futures::join::join;
-use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
-use embassy_stm32::spi;
-use embassy_stm32::time::{mhz, Hertz};
-use embassy_stm32::usb::Driver;
-use embassy_stm32::{bind_interrupts, peripherals, usb, Config};
-use embassy_time::Delay;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_nrf::usb::vbus_detect::HardwareVbusDetect;
+use embassy_nrf::usb::Driver;
+use embassy_nrf::{bind_interrupts, pac, peripherals, usb};
 
 use embassy_usb::control::{InResponse, OutResponse, Recipient, Request, RequestType};
 use embassy_usb::driver::{Endpoint, EndpointIn, EndpointOut};
 use embassy_usb::types::InterfaceNumber;
 use embassy_usb::{Builder, Handler};
 
-use embedded_graphics::{
-    pixelcolor::BinaryColor::On as Black,
-    prelude::*,
-    primitives::{Line, PrimitiveStyle},
-};
-use epd_waveshare::prelude::WaveshareDisplay;
-use epd_waveshare::{
-    epd4in2::{Display4in2, Epd4in2},
-    graphics::DisplayRotation,
-    prelude::*,
-};
-
 bind_interrupts!(struct Irqs {
-    USB_LP_CAN_RX0 => usb::InterruptHandler<peripherals::USB>;
+    USBD => usb::InterruptHandler<peripherals::USBD>;
+    POWER_CLOCK => usb::vbus_detect::InterruptHandler;
 });
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Hello World!");
 
-    let mut config = Config::default();
-    {
-        use embassy_stm32::rcc::*;
-        config.rcc.hse = Some(Hse {
-            freq: mhz(8),
-            mode: HseMode::Bypass,
-        });
-        config.rcc.pll = Some(Pll {
-            src: PllSource::HSE,
-            prediv: PllPreDiv::DIV1,
-            mul: PllMul::MUL9,
-        });
-        config.rcc.sys = Sysclk::PLL1_P;
-        config.rcc.ahb_pre = AHBPrescaler::DIV1;
-        config.rcc.apb1_pre = APBPrescaler::DIV2;
-        config.rcc.apb2_pre = APBPrescaler::DIV1;
-    }
-    let p = embassy_stm32::init(config);
+    let p = embassy_nrf::init(Default::default());
+    let clock: pac::CLOCK = unsafe { mem::transmute(()) };
 
-    let mut spi_config = spi::Config::default();
-    spi_config.frequency = Hertz(1_000_000);
-
-    let mut spi = spi::Spi::new(
-        p.SPI1, p.PB3, p.PB5, p.PB4, p.DMA1_CH3, p.DMA1_CH2, spi_config,
-    );
-    let cs = Output::new(p.PE0, Level::High, Speed::VeryHigh);
-    let busy = Input::new(p.PE1, Pull::None);
-    let dc = Output::new(p.PE2, Level::High, Speed::VeryHigh);
-    let rst = Output::new(p.PE3, Level::High, Speed::VeryHigh);
-
-    let mut delay = Delay;
-
-    let mut epd =
-        Epd4in2::new(&mut spi, cs, busy, dc, rst, &mut delay).expect("eink initalize error");
-    let mut display = Display4in2::default();
-
-    display.set_rotation(DisplayRotation::Rotate0);
-    // Use embedded graphics for drawing a line
-    let _ = Line::new(Point::new(0, 120), Point::new(0, 295))
-        .into_styled(PrimitiveStyle::with_stroke(Black, 1))
-        .draw(&mut display);
-
-    // Display updated frame
-    epd.update_frame(&mut spi, &display.buffer(), &mut delay);
-    epd.display_frame(&mut spi, &mut delay);
-
-    // Set the EPD to sleep
-    epd.sleep(&mut spi, &mut delay);
+    info!("Enabling ext hfosc...");
+    clock.tasks_hfclkstart.write(|w| unsafe { w.bits(1) });
+    while clock.events_hfclkstarted.read().bits() != 1 {}
 
     // Create the driver, from the HAL.
-    let driver = Driver::new(p.USB, Irqs, p.PA12, p.PA11);
+    let driver = Driver::new(p.USBD, Irqs, HardwareVbusDetect::new(Irqs));
 
     // Create embassy-usb Config
     let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
@@ -107,7 +53,7 @@ async fn main(_spawner: Spawner) {
     let mut bos_descriptor = [0; 256];
     let mut control_buf = [0; 64];
 
-    let mut led = Output::new(p.PE9, Level::High, Speed::Low);
+    let mut led = Output::new(p.P0_13, Level::High, OutputDrive::Standard);
     led.set_high();
 
     let mut handler = ControlHandler {
@@ -152,6 +98,7 @@ async fn main(_spawner: Spawner) {
                 match read_ep.read(&mut data).await {
                     Ok(n) => {
                         info!("Got bulk: {:a}", data[..n]);
+                        led.toggle();
                         // Echo back to the host:
                         write_ep.write(&data[..n]).await.ok();
                     }
@@ -179,7 +126,6 @@ impl Handler for ControlHandler {
     fn control_out<'a>(&'a mut self, req: Request, buf: &'a [u8]) -> Option<OutResponse> {
         // Log the request before filtering to help with debugging.
         info!("Got control_out, request={}, buf={:a}", req, buf);
-        info!("Interface: {}", self.if_num.0 as u16);
 
         // Only handle Vendor request types to an Interface.
         if req.request_type != RequestType::Vendor || req.recipient != Recipient::Interface {
